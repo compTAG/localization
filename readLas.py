1747edbb8f01f77a365dcbcffa16fb24303dcbbd
Clare DuVal <ckduval@hotmail.com>
2019-06-06 11:25:20 -0600

    Update readLas.py

--- readLas.py
+++ readLas.py
@@ -3,9 +3,22 @@ import numpy as np
 
 inFile = File('test.las', mode='r')
 
+#print("Examining Point Format: ")
+#pointformat = inFile.point_format
+#for spec in inFile.point_format:
+#    print(spec.name)
+
 print("X dim: ", inFile.X)
 print("Y dim: ", inFile.Y)
-print("Z dim: ", inFile.Z) 
+print("Z dim: ", inFile.Z)
+
+#print("Examining Header Format:")
+#headerformat = inFile.header.header_format
+#for spec in headerformat:
+#    print(spec.name)
+
+#Puts x,y,z coordinates into a 3d array
+#Coordinates = np.vstack((inFile.x, inFile.y, inFile.z)).transpose()
 
 I = inFile.Classification == 2
 
