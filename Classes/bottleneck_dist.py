98f3371462e1d5b6cb09cd8c87242ecf78b489c0
Clare DuVal <ckduval@hotmail.com>
2019-07-03 11:07:48 -0600

    Update bottleneck_dist.py

--- Classes/bottleneck_dist.py
+++ Classes/bottleneck_dist.py
@@ -14,11 +14,16 @@ class BottleneckDistances:
         # A single PCPDS object
         self.test_grid = test_grid
 
-    def naive_search_distances(self):
+    def naive_search_distances(self, num):
         found_idx = 'Error'
 
         # Set max bottlenect dist
         best_dist = 1.01
+
+        top_idx = []
+
+        for i in num:
+            top_idx.append((0, best_dist))
 
         # Generate persistance diagram to search for
         pd1 = self.test_grid.get_persistance_diagram()
@@ -30,13 +35,15 @@ class BottleneckDistances:
             # Get persistance diagram for i which should be an idx
             pd2 = parallelograms[i].get_persistance_diagram()
 
-            # Check bottleneck distance against current lowest, if it is lower, saves new distance and that idx value
+            # Check bottleneck distance against current lowest,
+            # if it is lower, saves new distance and that idx value
             result = .bottleneck_distance(pd1[0],pd2[0])
 
-            # Change this to save a list of top 5 indexes
-            if result < best_dist:
-                best_dist = result
-                found_idx = i
+            # Save a list of top 5 indexes, keep sorted based off the result dist
+            top_idx.append((i, result))
+            top_idx = sorted(top_idx, key=lambda x:x[1])
+            if len(top_idx) > num:
+                top_idx.pop(num-1)
 
         # Return idx of lowest bottleneck distance to search_idx
-        return found_idx
+        return top_idx
