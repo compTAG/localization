ef5996b472cf9a90733de7124391f5892871918b
GeoEngel <GeoEngel.z@gmail.com>
2019-07-09 22:55:22 -0600

    REFACTORING: Added file_manager & path_manager files
    -Added path_manager to handle pathing
    -Added file_manager to handle saving & loading of objects

--- Classes/file_manager.py
+++ Classes/file_manager.py
@@ -1,4 +1,18 @@
-# The FileManager object deals with knowledge of pathing/folder structure
+# The FileManager object deals with Saving and Loading Objects
+
+import os
+from pathlib import Path
 
 class FileManager:
     
+    def __init__(self):
+        # TODO: Add support for saving/loading objects with directory refferal
+        pass
+    
+    def save(object, path):
+        # TODO: Pickle stuff here
+        pass
+    
+    def load(path):
+        # TODO: Implement depickle stuff here
+        pass
---
+++ Classes/path_manager.py
@@ -0,0 +1,22 @@
+# The PathManager object deals with knowledge of pathing/folder structure
+
+import os
+from pathlib import Path
+
+class PathManager:
+    
+    def __init__(self):
+        
+        self.root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        self.cur_dir = None
+        
+    def set_cur_dir(dir_path):
+        self.cur_dir = dir_path
+        
+    def get_cur_dir():
+        # TODO: Check if cur_dir is None, or an invalid path, and then ask them to re-enter a valid path.
+        pass
+        
+    # TODO: Move functionality from PCPDS to here
+    
+        
