6527d19fc30cba37006189fdd6e6db883eda1c5a
Clare DuVal <ckduval@hotmail.com>
2019-07-12 16:11:32 -0600

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -5,6 +5,7 @@ from Classes.PCPDS_manager import PCPDS_Manager as pm
 import pptk
 import numpy as np
 import os.path
+import laspy
 
 path_manager = pm()
 dir = path_manager.get_path_manager().get_full_cur_dir("small_3_2019-07-12/")
@@ -13,9 +14,23 @@ file_name = "1001"
 
 pcpds_obj = fm.load(os.path.join(dir, file_name))
 point_cloud = pcpds_obj.get_point_cloud()
+for p in point_cloud:
+    print('X: ' + str(p[0]) + ', Y: ' + str(p[1]) + ', Z: ' + str(p[2]))
 print("Showing Visualizer:")
 P = np.random.rand(100,3)
-v = pptk.viewer(point_cloud.T)
-v.set(point_size=0.01)
+v = pptk.viewer(point_cloud)
+v.set(point_size=100.0)
 
 delay = input("Press any key to exit")
+
+# header = laspy.header.Header()
+# outfile = laspy.file.File("output.las", mode="w", header=header)
+# outfile.X = [1, 2, 3]
+# outfile.Y = [0, 0, 0]
+# outfile.Z = [10, 10, 11]
+# outfile.close()
+# https://gis.stackexchange.com/questions/158708/use-laspy-to-create-las-file-from-scratch-without-opening-an-existing-las-file-f
+
+
+#Get rid of e^ format
+#Scale it between 0 and 1
