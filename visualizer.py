666834b3974543c898445691786f3d957466f69f
Clare DuVal <ckduval@hotmail.com>
2019-07-12 14:25:14 -0600

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -5,9 +5,10 @@ from Classes.PCPDS_manager import PCPDS_Manager as pm
 import pptk
 import numpy as np
 
-pcpds_obj = fm.load("/home/geoengel/Documents/Undergraduate Research/localization/cell_collection/small_3_2019-07-12/1000")
+pcpds_obj = fm.load("/Users/clarekirkland/Documents/REU19/localization/cell_collection/small_3_2019-07-12/1011")
 point_cloud = pcpds_obj.get_point_cloud()
 print("Showing Visualizer:")
 P = np.random.rand(100,3)
 v = pptk.viewer(point_cloud)
+delay = input("Press any key to exit")
 print("Done running")
