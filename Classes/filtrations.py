562e5a9df22bfee36ac2fffd9402c06a75a09057
Luke <lukeask@gmail.com>
2019-07-12 10:59:35 -0600

    Lower and upper star working

--- Classes/filtrations.py
+++ Classes/filtrations.py
@@ -11,6 +11,14 @@ class Filtration:
         diagram = d.init_diagrams(m,f)
         return diagram
 
-    # TODO
-    def get_lower_star(self):
-        pass
+    def get_lower_star(point_cloud):
+        f =d.fill_freudenthal(point_cloud, True)
+        m = d.homology_persistence(f)
+        diagram = d.init_diagrams(m,f)
+        return diagram
+
+    def get_upper_star(point_cloud):
+        f = d.fill_freudenthal(point_cloud, False)
+        m = d.homology_persistence(f)
+        diagram = d.init_diagrams(m,f)
+        return diagram
--- test_filtrations.py
+++ test_filtrations.py
@@ -1,7 +1,21 @@
 from Classes.filtrations import Filtration
 import numpy as np
+import dionysus as d
 
-points = np.array([[0.0,0.0,0.0],[2.0,0.0,0.0],[0.0,2.0,0.0],[2.0,2.0,0.0],[3.0,1.0,0.0]])
+#points to test on
+points = np.array([[0.0,0.0,0.0],[2.0,0.0,0.0],[0.0,2.0,0.0],[2.0,2.0,0.0],[3.0,1.0,0.0],[0.0,0.0,7.0]])
 
+#get rips diagram
 rip = Filtration.get_rips_diagram
-print(rip(points, 1, 5.0))
+rfilt = rip(points, 1, 5.0)
+print(rfilt)
+
+#get lower star diagram
+star = Filtration.get_lower_star
+lsfilt = star(points)
+print(lsfilt)
+
+#print diagrams
+d.plot.plot_diagram(rfilt[0], show = True)
+
+d.plot.plot_diagram(lsfilt[0], show = True)
