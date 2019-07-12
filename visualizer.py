191f950b80edcf75e051a431418e3762f03e21ee
GeoEngel <GeoEngel.z@gmail.com>
2019-07-12 14:30:44 -0600

    Visualizer update

--- visualizer.py
+++ visualizer.py
@@ -5,10 +5,16 @@ from Classes.PCPDS_manager import PCPDS_Manager as pm
 import pptk
 import numpy as np
 
-pcpds_obj = fm.load("/Users/clarekirkland/Documents/REU19/localization/cell_collection/small_3_2019-07-12/1011")
+path_manager = pm()
+dir = path_manager.get_path_manager().get_full_cur_dir("small_3_2019-07-12/")
+
+file_name = "1001"
+
+pcpds_obj = fm.load(os.path.join(dir, file_name))
 point_cloud = pcpds_obj.get_point_cloud()
 print("Showing Visualizer:")
 P = np.random.rand(100,3)
 v = pptk.viewer(point_cloud)
+v.set(point_size=0.1)
+
 delay = input("Press any key to exit")
-print("Done running")
