a44a058b91e7c4856df07d4d0d28bf22cdfafde1
Clare DuVal <ckduval@hotmail.com>
2019-07-18 13:23:41 -0600

    Update experiment6.py

--- experiment6.py
+++ experiment6.py
@@ -33,18 +33,31 @@ def main():
         [test_pcpds, test_idx] = random_test_grid(collection_path)
 
         (X, Y, Z) = test_pcpds.get_xyz(test_idx)
+        [dimX, dimY] = test_pcpds.get_dimensions #**
 
-        results = [_,_]
+        results = []
 
         pcpds_manager = PCPDS_Manager()
 
-        #Change this to loop over X+-1 and Y+-1
-        temp_pcpds = pcpds_manager.get_pcpds(las_obj.find_idx(X-1, Y))
+        #Change this to loop over X+-1 and Y+-1, check exists
+        slide_pcpds = pcpds_manager.get_pcpds(las_obj.find_idx(X+1, Y))
 
         #Add for loop over 0.1 to 1 and to bottleneck distance to average in results
+        for overlay in range(10):
+            begin_X = (float(overlay)/10.0 * dimX) + (dimX * X)
+            end_X = (float(overlay)/10.0 * dimX) + (dimX * X) + dimX
 
+            for pc in test_pcpds.point_cloud:
+                #if point is within bounds of beginX and endX regardless of Y coord, add to temp_pcpds
+                pass
 
-        n_results = 4 #num_results(partition)
+            for pc in slide_pcpds.point_cloud:
+                #if point is within bounds of beginX and endX regardless of Y coord, add to temp_pcpds
+                pass
+
+            results[overlay] = #bottleneck temp_pcpds against test_pcpds
+
+        #Repeat above for X-1, Y+-1
 
         # Calculate bottleneck distance, print n_result matches
 
