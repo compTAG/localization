4e87428468259eb439ff2e6c651f25807c04cb9b
Luke <lukeask@gmail.com>
2019-07-23 14:00:11 -0600

    changed to not include init point

--- Classes/menu.py
+++ Classes/menu.py
@@ -82,17 +82,15 @@ class menu:
         print(test_pcpds.point_cloud)
         print(slide_pcpds.point_cloud)
 
-        points_in_bounds = np.array([_,_,_]) # pop this later
+        points_in_bounds = np.array([0,0,0]) # pop this later
         union = np.vstack((test_pcpds.point_cloud,slide_pcpds.point_cloud))
         for i in range(len(union)):
             if low_x_bound <= union[i][0] and union[i][0] < high_x_bound and low_y_bound <= union[i][1] and union[i][1] < high_y_bound:
                 points_in_bounds = np.vstack((union[i],points_in_bounds))
-
         ret_pcpds = pcpds(-1, (high_x_bound-low_x_bound, high_y_bound-low_y_bound))
         #points_in_bounds = np.delete(points_in_bounds,len(points_in_bounds)-1, 0)
-        ret_pcpds.set_point_cloud(points_in_bounds)
+        ret_pcpds.set_point_cloud(points_in_bounds[1:])
 
 
         print(ret_pcpds.point_cloud)
-
         return ret_pcpds
