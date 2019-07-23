059b4c04224a5dd96dcf7204c1e9bf4c42a6cacb
GeoEngel <GeoEngel.z@gmail.com>
2019-07-23 14:50:49 -0600

    Created modifiers file for altering pcpds object data
    -moved rotation methods into this class
    -added section for introducing noise

--- modifiers.py
+++ modifiers.py
@@ -1,23 +1,13 @@
+# This file deals with methods that alter/modify pcpds objects in some way, usually for testing
+
 from Classes.PCPDS import PCPDS
 from Classes.PCPDS_manager import PCPDS_Manager
 import numpy as np
 import math
 
-# The purpose of this class is to hold the test cases for use of demonstrating tests
+# Noise functions: Add random/specified noise to point clouds for comparison
 
-# Chooses a random section from the las grid, rotates it, and sees if it can find it in the grid again using rips filtration methodology.
-class rotated_section_test:
-    
-    def __init__(self, pcpds):
-        
-        # Begin by rotating the pcpds's point cloud
-        
-        # Process the pcpds's point cloud into a persistance diagram
-        
-        # Use Bottle neck distance to try and find the closest match
-        pass
-        
-### Rotates a passed in pcpds's point cloud and regenerates it's persistance diagram.
+# point cloud rotation functions:
 
 def rotate_section(pcpds, theta_x, theta_y, theta_z):
     result = rotate_section_x(pcpds, theta_x)
@@ -61,18 +51,3 @@ def rotate_section_z(pcpds, theta):
     
     pcpds.set_point_cloud(new_point_cloud)
     return pcpds
-        
-def print_pointcloud(pcpds):
-    print(pcpds.get_point_cloud())
-
-def main():
-    pcpds_manager = PCPDS_Manager()
-    pcpds_manager.set_collection_dir("tiny_10_2019-07-23")
-    pcpds = pcpds_manager.get_pcpds("1010601")
-    print_pointcloud(pcpds)
-    result = rotate_section(pcpds, 20, 52, 93)
-    print("RESULT:")
-    print_pointcloud(pcpds)
-    
-main()
-
