ee4b8b9833cb371fab46e11d0e609914f830d094
GeoEngel <GeoEngel.z@gmail.com>
2019-07-26 11:10:03 -0600

    Making additions to bottleNeck_comparison for additional functionality

--- bottleNeck_comparison.py
+++ bottleNeck_comparison.py
@@ -49,9 +49,39 @@ print("Ready to process, how manny n_nearest results would you like?")
 # TODO: Validate that n_results is a valid number for the current dataset.
 n_results = menu.get_int_input()
 
-rand_pcpds = pcpds_manager.get_random_pcpds()
+# TODO: Ask the user if they want to specify a pcpds or select a random one.
+print("Enter a specific pcpds number, or enter 'R' for a random one.")
+specification = menu.get_input("PCPDS Num:")
+
+pcpds = None
+valid_pcpds_found = False
+
+while not valid_pcpds_found:
+    if specification.lower() is "r":
+        pcpds = pcpds_manager.get_random_pcpds()
+        valid_pcpds_found = True
+    else:
+        try:
+            # Checks if the specification is an integer.
+            int(specification)
+            # Attempt to load in specified pcpds
+            try:
+                pcpds = pcpds_manager.get_pcpds(specification)
+                valid_pcpds_found = True
+            except:
+                # Dosen't exist?
+                print("pcpds file for cell_ID:", specification, "dosen't exist.")
+        except:
+            print("Invalid input entered of type:", specification,":", type(specification))
+        
+        specification = menu.get_input("PCPDS Num:")
+
+# TODO: Select modifications to apply to the rand_pcpds file now
+print("PCPDS Selected: ")
+
+
 # Calculated closest n matching bottleneck distances.
-closest_matches  = bottleneck_distances.search_distances(n_results, rand_pcpds.get_persistance_diagram(), valid)
+closest_matches  = bottleneck_distances.search_distances(n_results, pcpds.get_persistance_diagram(), valid)
 
 wb = Workbook()
 excel_sheet = wb.add_sheet('Bottle_Neck_Distance_Comparison')
@@ -60,14 +90,14 @@ excel_sheet.write(0, 0, "Closest_" + str(n_results) + "_BD_Matches")
 excel_sheet.write(0, 1, "Bottle_Neck_Distance")
 
 excel_sheet.write(0, 2, "Cell_ID_Compared_Against:")
-excel_sheet.write(1, 2, str(rand_pcpds.get_cellID()))
+excel_sheet.write(1, 2, pcpds.get_cellID())
 
 iter = 1
 for idx in closest_matches:
     
     # Write results .xls file
-    excel_sheet.write(iter, 0, str(idx[0][:-5]))
-    excel_sheet.write(iter, 1, str(idx[1]))
+    excel_sheet.write(iter, 0, idx[0][:-5])
+    excel_sheet.write(iter, 1, idx[1])
     iter = iter + 1
 
 wb.save(os.path.join("results", pcpds_manager.get_path_manager().get_cur_dir()) + '.xls')
