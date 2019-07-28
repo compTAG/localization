424cf1d43990eca0cfed02231ba39e3420df1a06
GeoEngel <GeoEngel.z@gmail.com>
2019-07-28 03:39:05 -0600

    Put a Band aid on the issue of repeating bottleneck distances for data

--- Classes/bottleneck_dist.py
+++ Classes/bottleneck_dist.py
@@ -10,24 +10,28 @@ from Classes.menu import menu
 def get_distances(filt1, filt2):
     return d.bottleneck_distance(filt1[0],filt2[0])
 
-def get_collectionset(dir_path):
-    return fm.find_files(dir_path, '.json')
-
 def search_distances(num, searchfilt, collection_path):
-    cell_IDs = get_collectionset(collection_path)
+    cell_IDs = fm.find_files(collection_path, '.json')
     top_idx = []
     tracker = 0
     for id in cell_IDs:
         pcpds = fm.load(os.path.join(collection_path, id))
 
-        testfilt = pcpds.get_persistance_diagram()
+        result = get_distances(pcpds.get_persistance_diagram(), searchfilt)
+        #print("CURRENT CELL_ID:", id)
+        if len(top_idx) < num:
+            top_idx.append([id, result])
+            top_idx = sorted(top_idx, key=lambda x:x[1])
+        elif top_idx[-1][1] > result and not [id, result] in top_idx:
+            #print("REPLACING:",top_idx[-1], "LIST LEN:",len(top_idx), top_idx)
+            top_idx.pop(-1)
+            top_idx.append([id, result])
+            #print("\nBEFORE SORT:", top_idx)
+            top_idx = sorted(top_idx, key=lambda x:x[1])
+            #print("AFTER SORT:", top_idx)
+            #print("WITH:", id, result, "LIST LEN:", len(top_idx))
 
-        result = get_distances(testfilt, searchfilt)
-
-        top_idx.append([id, result])
-        top_idx = sorted(top_idx, key=lambda x:x[1])
-        if len(top_idx) > num:
-            top_idx.pop(num-1)
+    
         menu.progress(tracker, len(cell_IDs), 'Processing Search_Disantce...')
         tracker = tracker + 1
     print("\n")
