4f79aa597e72be0f012fcecca2d7c2a2be139819
GeoEngel <GeoEngel.z@gmail.com>
2019-07-29 17:11:49 -0600

    More data & rotation sanity check outline.

---
+++ sanityCheck_rotatepcpds.py
@@ -0,0 +1,14 @@
+# We rotate the point cloud, recompute it's persistence diagram, and compare it's persistance diagram to the origional.
+# They should be identical, therefore showing the angle of approach does not matter using this method of TDA.
+
+# TODO: Enter & verify dir
+# TODO: Enter & verify file name
+
+# TODO: Load in pcpds object from dir & name
+# Verify the pcpds object has a persistence diagram. if not, repeat this process.
+
+# TODO: Make a copy of pcpds object & rotate it's point cloud
+
+# TODO: Regenerate rotated_pcpds's persistence diagram
+
+# Compute bottleneck distance & check it is near 0. (within margin of error from rotation)
