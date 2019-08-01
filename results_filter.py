71735dbde1badc86d57f4a7b8a21257742c4cc9a
GeoEngel <GeoEngel.z@gmail.com>
2019-08-01 11:56:58 -0600

    results_filter clean up & additional output.

--- results_filter.py
+++ results_filter.py
@@ -35,35 +35,36 @@ for file_name in file_names:
     if ".~lock" in file_name:
         print("\nSkipping .~lock file:", file_name)
     else:
-        # try:
-        # To open Workbook
-        rb = xlrd.open_workbook(os.path.join(pm.get_collection_dir(), file_name))
-        sheet = rb.sheet_by_index(0) 
-        
-        # Store results in other workbook
-        for n in range(1,11):
-            val = sheet.cell_value(n, 1)
-            excel_sheet.write(n-1, iter, val)
-        
-        # Check if the idx of the closest Bottleneck distance is the same as the one it was compared against:
-        closest_idx = int(sheet.cell_value(1, 0))
-        searched_idx = int(sheet.cell_value(1, 2))
-        print("\nClosest:", closest_idx, "Searched:", searched_idx, "\n")
-        if closest_idx == searched_idx:
-            excel_sheet.write(10, iter, "yes")
-        else:
-            excel_sheet.write(10, iter, "no")
+        try:
+            # To open Workbook
+            rb = xlrd.open_workbook(os.path.join(pm.get_collection_dir(), file_name))
+            sheet = rb.sheet_by_index(0) 
             
-        iter = iter + 1
-        
-        # Limit it to 255 since that is the max for excel files.
-        if iter > 255:
-            break
-        # except:
-        #     print("\nERROR Invalid file:", file_name)
+            # Store results in other workbook
+            for n in range(1,11):
+                val = sheet.cell_value(n, 1)
+                excel_sheet.write(n-1, iter, val)
+            
+            # Check if the idx of the closest Bottleneck distance is the same as the one it was compared against:
+            closest_idx = int(sheet.cell_value(1, 0))
+            searched_idx = int(sheet.cell_value(1, 2))
+            
+            if closest_idx == searched_idx:
+                excel_sheet.write(10, iter, "yes")
+            else:
+                excel_sheet.write(10, iter, "no")
+                
+            iter = iter + 1
+            
+            # Limit it to 255 since that is the max for excel files.
+            if iter > 255:
+                print("\nLimiting results folder to excel file capacity of 255 columns for results.")
+                break
+        except:
+            print("\nERROR Invalid file:", file_name)
     menu.progress(iter, len(file_names), "Processed: "+file_name)
 
 wb.save(os.path.join(pm.get_collection_dir(), 'compiled_results.xls'))
-menu.progress(1, 1, "Finished compiling result data.")
+menu.progress(1, 1, "Finished compiling result data.\n")
     
 # smaller_25 lower star results
